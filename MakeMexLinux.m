b39cce97bfe05b316d5bf481c015411ab4667301
Olivier Dufour <odufour@mathworks.com>
2023-08-09 13:14:28 +0200

    feat: Convert MakeMexLinux to a function + add support to 4.48 and 5.0 with the same syntax as for Windows

--- MakeMexLinux.m
+++ MakeMexLinux.m
@@ -1,3 +1,4 @@
+function MakeMexLinux(glpkVersion, options)
 % ----------------------------------------------------------------------------------------------- %
 % MakeMex - Generating Linux x64 MEX File for GLPK
 % Generates the MEX file of GLPK for Linux x64 system by downloading GLPK
@@ -12,35 +13,31 @@
 %   Release Notes:
 %   -   1.0.000     01/05/2020  Royi Avital
 %       *   First release version.
+%
+% MexApi
+%   * -R2018a : Large Arrays (64 Bit Indices), Interleaved Complex Arrays
+%   * -R2017b : Large Arrays (64 Bit Indices), Separate Complex Arrays
+%   * -largeArrayDims : Large Arrays (64 Bit Indices), Separate Complex Arrays (Basically like R2017b)
+%   * -compatibleArrayDims : 32 Bit Indices, Separate Complex Arrays
+%
 % ----------------------------------------------------------------------------------------------- %
 
+arguments
+    glpkVersion             (1,1)       string      {mustBeMember(glpkVersion, ["4.48","4.65","5.0"])}    = "4.48";
+    options.MexApi          (1,1)       string      {mustBeMember(options.MexApi, ["-R2017b","-R2018a","-largeArrayDims","-compatibleArrayDims"])} = "-R2017b";
+    options.UseCompFlags    (1,1)       logical     = false;
+end
 
 %% Setting Environment Parameters
 
-close('all');
-clear('all');
-clc();
-
-FALSE   = 0;
-TRUE    = 1;
-
-OFF     = 0;
-ON      = 1;
-
-FILE_SEP = filesep();
-PATH_SEP = pathsep();
-
-MEX_API_R2018A              = '-R2018a'; %<! Large Arrays (64 Bit Indices), Interleaved Complex Arrays
-MEX_API_R2017B              = '-R2017b'; %<! Large Arrays (64 Bit Indices), Separate Complex Arrays
-MEX_API_LARGE_ARRAY         = '-largeArrayDims'; %<! Large Arrays (64 Bit Indices), Separate Complex Arrays (Basically like R2017b)
-MEX_API_COMPATIBLE_ARRAY    = '-compatibleArrayDims'; %<! 32 Bit Indices, Separate Complex Arrays
-
-glpkFileName    = 'glpk-4.65.tar.gz';
-glpkUrl         = 'https://ftp.gnu.org/gnu/glpk/glpk-4.65.tar.gz';
-glpkFolder      = 'glpk-4.65';
-mexFileName     = 'glpkcc.cpp';
-libFileName     = 'libglpk.a';
-
+glpkFileName    = "glpk-" + glpkVersion + ".tar.gz";
+glpkUrl         = "https://ftp.gnu.org/gnu/glpk/glpk-" + glpkVersion + ".tar.gz";
+glpkFolder      = "glpk-" + glpkVersion;
+if glpkVersion == "4.48"
+    mexFileName     = 'glpkcc_4.48.cpp';
+else
+    mexFileName     = 'glpkcc.cpp';
+end
 
 %% User Settings
 
@@ -48,27 +45,20 @@ libFileName     = 'libglpk.a';
 cCompFlags      = '-O2 -mavx2';
 mexCompFlags    = '-Ofast -mavx2';
 
-useCompFlags    = OFF;
-% Add support for arrays larger than ~2^31 elements (MEX_API_R2018A,
-% MEX_API_R2018A, MEX_API_LARGE_ARRAY)
-mexApi          = MEX_API_R2017B;
-
-
 %% Inner Settings
 
-glpkBuildFolder     = [glpkFolder, FILE_SEP];
-glpkSrcFolder       = [glpkFolder, FILE_SEP, 'src', FILE_SEP];
-workingFolderPath   = [pwd(), FILE_SEP];
-
+glpkBuildFolder = glpkFolder;
+glpkSrcFolder   = fullfile(glpkFolder, 'src');
+libFileName     = 'libglpk.a';
 
 %% Downloading Data & Setting Environment
 
-disp(['Downloading GLPK 4.65 from Official Site']);
-disp([' ']);
-glpkFilePath    = websave(glpkFileName, glpkUrl);
-disp(['Unpacking the TAR File']);
-disp([' ']);
-cFileNames      = untar(glpkFileName);
+disp("Downloading GLPK " + glpkVersion + " from Official Site");
+disp(" ");
+websave(glpkFileName, glpkUrl);
+disp("Unpacking the TAR File");
+disp(" ");
+untar(glpkFileName);
 
 currFolder = cd(glpkBuildFolder);
 
@@ -76,49 +66,47 @@ currFolder = cd(glpkBuildFolder);
 %% Compilation
 
 % Compiling the GLPK Library
-disp(['Compiling the GLPK Library']);
-disp([' ']);
-if(useCompFlags == ON)
+disp("Compiling the GLPK Library");
+disp(" ");
+if(options.UseCompFlags)
     cFlags = cCompFlags;
 else
-    cFlags = [];
+    cFlags = '';
 end
 
-system('./configure');
+[status,log] = system('./configure');
+assert(status == 0, "Failed to configure with message:\n%s", log)
 % The flag '-fPIC' is a must in order to statically link the EMX to the
 % GLPK Library
-system(['make CFLAGS="$CFLAGS -fPIC ', cFlags, '"']);
+[status,log] = system(['make CFLAGS="$CFLAGS -fPIC ', cFlags, '"']);
+assert(status == 0, "Failed to compile with message:\n%s", log)
 
-disp(['Checking the Compilation of the GLPK Library: If it shows "OPTIMAL LP SOLUTION FOUND" all worked!']);
-disp([' ']);
-system(['make check']);
+disp("Checking the Compilation of the GLPK Library: If it shows ""OPTIMAL LP SOLUTION FOUND"" all worked!");
+disp(" ");
+[status,log] = system('make check');
+assert(status == 0, "Failed to check with message:\n%s", log)
 
 cd(currFolder);
 
-if(useCompFlags == ON)
+if(options.UseCompFlags)
     mexCompFlags      = ['CFLAGS=$CFLAGS ', mexCompFlags];
 else
-    mexCompFlags      = ['CFLAGS=$CFLAGS'];
+    mexCompFlags      = 'CFLAGS=$CFLAGS';
 end
-includeFolder   = ['-I', glpkSrcFolder];
-libFilePath     = [glpkSrcFolder, FILE_SEP, '.libs', FILE_SEP, libFileName];
+includeFolder   = ['-I', char(glpkSrcFolder)];
+libFilePath     = char(fullfile(glpkSrcFolder, '.libs', libFileName));
 
-disp(['Compiling the MEX File']);
-disp([' ']);
+disp("Compiling the MEX File");
+disp(" ");
 
-mex('-v', mexApi, mexCompFlags, includeFolder, mexFileName, libFilePath);
-% mex 'glpkcc.cpp' -Lglpk-4.65/src -Iglpk-4.65/src -lglpk
-% 
-% mex 'glpkcc.cpp' 'glpk-4.65/src/.libs/libglpk.a' -Iglpk-4.65/src
-% mex -Iglpk-4.65/src glpkcc.cpp glpk-4.65/src/.libs/libglpk.a
-
+mex('-v', options.MexApi, mexCompFlags, includeFolder, mexFileName, libFilePath);
 
 % Verify
-disp(['Verifying the MEX File: If it shows "MEX interface to GLPK Version 4.65" all worked!']);
-disp([' ']);
+disp("Verifying the MEX File: If it shows ""MEX interface to GLPK Version " + glpkVersion + """ all worked!");
+disp(" ");
 glpkcc(); %<! Should display the version
 
-clear('all'); %<! In order to remove the MEX from memory
+clear("glpkcc"); %<! In order to remove the MEX from memory
 
 
 %% Restore Defaults
